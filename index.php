15c1d645fe17b12dd485a7aa6d8fa80af3b90038
Talitha Almendro <004493011@itaud.des.ihf>
2021-03-03 16:03:03 -0300

    Versionamento para meu primeiro site em Prudção

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title>Digi-Evolution</title>
-</head>
-<body>
-
-<h1>Digi-Evolution</h1>
-<p>Monstrinhos Digitais evoluindo o seu negocio</p>
-
-</body>
+   <head>
+      <title>Digi-Evolution</title>
+   </head>
+   <body>
+      <h1>Digi-Evolution</h1>
+      <p>Monstrinhos Digitais evoluindo o seu negocio</p>
+      <div style="width:100%;display:flex;height: 80px">
+         <div style="width:50%;background:blue"></div>
+         <div style="width:50%;background:red"></div>
+      </div>
+   </body>
 </html>
